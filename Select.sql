6b706c7b40b2b3c40a3a2b0423a9647ff3cd5a7f
G2p1 <andrii.tiuitunnyk@gmail.com>
2023-04-28 01:55:21 +0300

    Add: select query for task 1

---
+++ Select.sql
@@ -0,0 +1,9 @@
+﻿-- Task 1
+
+select an_name, an_price 
+from dbo.Analysis as an 
+inner join dbo.Orders as ord 
+on an.an_id=ord.ord_an
+where (ord.ord_datetime >= '2020-02-05') and (ord.ord_datetime <= '2020-02-12');
+
+-- Task 2
