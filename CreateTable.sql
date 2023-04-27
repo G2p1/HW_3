bed95c609f247e288c51b3fb96d22fa99d6f5090
G2p1 <andrii.tiuitunnyk@gmail.com>
2023-04-28 01:22:08 +0300

    Init: initialization db

---
+++ CreateTable.sql
@@ -0,0 +1,32 @@
+﻿use my_DB2
+
+create table [dbo].[Groups]
+(
+	[gr_id] int not null,
+	[gr_name] varchar(30) not null,
+	[gr_temp] float not null,
+
+	constraint [PK_Groups] primary key (gr_id)
+)
+
+create table [dbo].[Analysis]
+(
+	[an_id] int not null,
+	[an_name] varchar(30) not null,
+	[an_cost] float not null,
+	[an_price] float not null,
+	[an_group] int not null,
+
+	constraint [PK_Analysis] primary key (an_id),
+	constraint [FK_Analysis_Groups] foreign key (an_group) references [dbo].[Groups](gr_id)
+)
+
+create table [dbo].[Orders]
+(
+	[ord_id] int not null,
+	[ord_datetime] datetime2 not null,
+	[ord_an] int not null,
+
+	constraint [PK_Orders] primary key (ord_id),
+	constraint [FK_Orders_Analysis] foreign key (ord_an) references [dbo].[Analysis](an_id)
+)
---
+++ InsertTable.sql
@@ -0,0 +1,18 @@
+﻿use my_DB2
+
+insert into dbo.Orders(ord_id, ord_datetime, ord_an) values
+(1,'2020-02-05', 1),
+(2,'2020-02-08', 1),
+(3,'2021-08-23', 1),
+(4,'2022-12-01', 2),
+(5,'2023-04-26', 3)
+
+insert into dbo.Groups(gr_id, gr_name, gr_temp) values
+(1, 'long time detected', -12.5),
+(2, 'short tome detected', 18.7)
+
+insert into dbo.Analysis(an_id, an_name, an_cost, an_price, an_group) values
+(1, 'ease analis', 1000, 800, 2),
+(2, 'blood analis', 3000, 2900, 1),
+(3, 'corona analisys', 1300, 1200, 2)
+
