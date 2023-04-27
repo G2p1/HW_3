3a4c59cb15bbec61a0baadd8177b8d3d35fdadf4
G2p1 <andrii.tiuitunnyk@gmail.com>
2023-04-28 02:58:11 +0300

    Add: solution in Select.sql for task2 and screenshot with output

--- Select.sql
+++ Select.sql
@@ -7,3 +7,12 @@ on an.an_id=ord.ord_an
 where (ord.ord_datetime >= '2020-02-05') and (ord.ord_datetime <= '2020-02-12');
 
 -- Task 2
+
+select distinct count(ord_id) over(partition by month(ord_datetime)) as SelledTest,
+month(ord_datetime) as PerMonth, YEAR(ord_datetime) as perYear
+from Orders as o 
+inner join Analysis as an 
+on o.ord_an = an.an_id 
+inner join Groups as gr 
+on an_group=gr_id 
+group by ord_id,ord_datetime;
